b2676787ea314002def584fa82f07e47fdd144d0
Hanif Maliki <hanifmaliki97@gmail.com>
2022-06-25 18:53:44 +0700

    Heroku2

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"os"
 	"the-movie-backend/movies"
 	"the-movie-backend/tv"
 
@@ -10,10 +12,16 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.GET("/", home)
 	router.GET("/movies/getTopRated/:page", movies.GetTopRated)
 	router.GET("/movies/getUpcoming/:page", movies.GetUpcoming)
 	router.GET("/tv/getTopRated/:page", tv.GetTopRated)
 	router.GET("/tv/getPopular/:page", tv.GetPopular)
 
-	router.Run("localhost:8080")
+	port := os.Getenv("PORT")
+	router.Run(":" + port)
+}
+
+func home(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Welcome to Hanif's Api"})
 }
